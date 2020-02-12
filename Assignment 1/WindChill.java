56ed00e99526febc4a645c027dbee47b361a5344
cjfghd21 <60458858+cjfghd21@users.noreply.github.com>
2020-02-11 21:14:36 -0500

    Add files via upload

--- Assignment 1/WindChill.java
+++ Assignment 1/WindChill.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 import java.lang.Math;
-
+/**
+ * This program calculates wind chill from 
+ * two values temperature(F) and wind speed (mph)
+ * given from the user.
+ * @author Cheolhong Ahn
+ * 
+ */
 public class WindChill
 {
 
@@ -22,12 +28,16 @@ public class WindChill
 		System.out.println("Please enter a wind speed in mph between 5 and 60 inclusively.");
 		windSpeed = userInput.nextDouble();
 		
-		double windChill = WIND_CHILL_VAL_ONE + WIND_CHILL_VAL_TWO * temperature - WIND_CHILL_VAL_THREE * Math.pow(windSpeed, WIND_CHILL_VAL_FOUR) + WIND_CHILL_VAL_FIVE * temperature *  Math.pow(windSpeed, WIND_CHILL_VAL_FOUR);
-		
-		System.out.print("The wind chill is: " + windChill + " degrees Fahrenheit \n\n");
-		
+		if(temperature >= -45 && temperature <= 45 && windSpeed >= 5 && windSpeed <= 60)
+		{	
+			double windChill = WIND_CHILL_VAL_ONE + WIND_CHILL_VAL_TWO * temperature - WIND_CHILL_VAL_THREE * Math.pow(windSpeed, WIND_CHILL_VAL_FOUR) + WIND_CHILL_VAL_FIVE * temperature *  Math.pow(windSpeed, WIND_CHILL_VAL_FOUR);
+			System.out.print("The wind chill is: " + windChill + " degrees Fahrenheit \n\n");
+		}
+		else
+		{
+			System.out.println("Please enter valid value of temperature(F) between -45 to 45 and windspeed betwee 5 to 60(mph)");
+		}
 		System.out.print("Programmer Name:" + programmerName);
-		
 		userInput.close();
 	}
 }
